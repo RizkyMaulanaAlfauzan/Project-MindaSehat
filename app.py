82019cef864af5f71c8f55ad05a4e33c8c8cb3f8
rzadwiptri <reza.210170061@mhs.unimal.ac.id>
2023-12-11 19:47:41 +0700

    commit dari eja

--- app.py
+++ app.py
@@ -15,5 +15,17 @@ app = Flask(__name__)
 def home():
     return render_template("buatJadwal.html")
 
+@app.route('/registrasi', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        nama = request.form['nama']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if password != confirm_password:
+            return "Password dan konfirmasi password tidak cocok. Silakan coba lagi."
+        return "Registrasi berhasil untuk nama: {}, email: {}".format(nama, email)
+    return render_template('registrasi.html')
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
