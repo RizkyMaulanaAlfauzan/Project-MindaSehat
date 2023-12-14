f53d7f7d9291bd1c2aac06b1200264033ed541fb
rzadwiptri <reza.210170061@mhs.unimal.ac.id>
2023-12-14 19:10:09 +0700

    registrasi lagi

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from flask import (
     request, 
     session, 
     redirect,
-    url_for
+    url_for,
 ) 
+from werkzeug.exceptions import BadRequestKeyError
   
 import uuid
 from pymongo import MongoClient
@@ -19,7 +20,6 @@ import os
 from os.path import join, dirname
 
 app = Flask(__name__)
-
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -52,7 +52,7 @@ def sign_up():
     msg = request.args.get("msg")
     return render_template("registrasi.html", msg=msg)
 
-@app.route("/sign_up/user", methods=["POST"])
+@app.route("/sign-up-user", methods=["POST"])  
 def sign_up_user():
     theId = f"{uuid.uuid1()}"
     username_receive = request.form["name"]
@@ -60,13 +60,27 @@ def sign_up_user():
     email_info = db.user.find_one({"email": email_receive})
     password_receive = request.form["password"]
     confirm_password_receive = request.form["confirm_password"]
-    if email_info:
-        return redirect(url_for("sign_up", msg="Email Anda Sudah Terdaftar"))
-    if len(password_receive) < 8:
-        return redirect(url_for("sign_up", msg="Password Anda Terlalu Pendek"))
+
+    # Periksa apakah input email memiliki nama yang benar
+    if "email" not in request.form:
+        raise BadRequestKeyError("Parameter 'email' tidak ditemukan")
+
+    # Periksa apakah input email dikirimkan dengan metode POST
+    if request.method != "POST":
+        raise BadRequestKeyError("Metode HTTP harus POST")
+
+    # Periksa apakah data email tidak diubah atau dihapus oleh middleware atau dekorator
+    if not email_receive:
+        raise BadRequestKeyError("Parameter 'email' tidak boleh kosong")
+
+    # Periksa apakah kata sandi dan konfirmasi kata sandi cocok
     if password_receive != confirm_password_receive:
         return redirect(url_for("sign_up", msg="Password Anda Tidak valid, Mohon di cek kembali!!!"))
+
+    # Hash kata sandi
     password_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
+
+    # Simpan data pengguna ke database
     doc = {
         "uuid": theId,
         "username": username_receive,
@@ -74,35 +88,9 @@ def sign_up_user():
         "password": password_hash,  # password
     }
     db.user.insert_one(doc)
-    # return jsonify({"result": "success"})
+
+    # Alihkan ke halaman login
     return redirect(url_for("login", msg="Silahkan Login"))
-
-
-@app.route("/sign_up/psikologis", methods=["POST"])
-def sign_up_psikologis():
-    theId = f"{uuid.uuid1()}"
-    username_receive = request.form["name"]
-    email_receive = request.form["email"]
-    email_info = db.psikologis.find_one({"email": email_receive})
-    password_receive = request.form["password"]
-    confirm_password_receive = request.form["confirm_password"]
-    if email_info:
-        return redirect(url_for("sign_up", msg="Email Anda Sudah Terdaftar"))
-    if len(password_receive) < 8:
-        return redirect(url_for("sign_up", msg="Password Anda Terlalu Pendek"))
-    if password_receive != confirm_password_receive:
-        return redirect(url_for("sign_up", msg="Password Anda Tidak valid, Mohon di cek kembali!!!"))
-    password_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
-    doc = {
-        "uuid": theId,
-        "username": username_receive,
-        "email": email_receive,
-        "password": password_hash,  # password
-    }
-    db.psikologis.insert_one(doc)
-    # return jsonify({"result": "success"})
-    return redirect(url_for("login", msg="Silahkan Login"))
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
